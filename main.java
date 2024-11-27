4d070690a61b80bbac095f6177624b0fc515596f
Vivaan Shah <shahvivaan7@gmail.com>
2024-11-26 21:47:47 -0500

    Created instance of interface

--- main.java
+++ main.java
@@ -2,3 +2,21 @@
 interface AddNumbers {
     int sum(int start, int end);
 }
+
+public class main {
+    public static void main(String[] args) {
+
+        int start = Integer.parseInt(args[0]);
+        int end = Integer.parseInt(args[1]);
+
+        AddNumbers add = (int a, int b) -> {
+            int sum = 0;
+            for (int i = a; i <= b; i++) {
+                sum += i;
+            }
+            return sum;
+        };
+
+        System.out.println("Sum: " + add.sum(start, end));
+    }
+}
